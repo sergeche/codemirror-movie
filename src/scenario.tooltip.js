ff46c1dcef89c8fb2ef1ecec7a8a37689a212c80
Sergey Chikuyonok <serge.che@gmail.com>
2013-02-21 23:12:06 +0400

    Minor improvements, updated Emmet core

--- src/scenario.tooltip.js
+++ src/scenario.tooltip.js
@@ -119,7 +119,7 @@ CodeMirror.scenarioTooltip = (function() {
 				style[cssTransform] = prefix + 'scale(' + pos + ')';
 			},
 			complete: function() {
-//				style[cssTransform] = 'none';
+				style[cssTransform] = 'none';
 				lastTween = null;
 				if (options.onComplete) {
 					options.onComplete(elem);
